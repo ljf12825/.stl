4d115b212e0e6769e1c2a6fa99b821bcee53bdde
Jeff <2548278761@qq.com>
2025-04-22 11:35:33 +0800

    update vector.cpp

--- C++_STL/Vector.cpp
+++ C++_STL/Vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 //std::vector
 //特点
@@ -44,9 +45,28 @@ class _vector_
 //某个操作在大多数情况下很快，只有偶尔慢，但是把多次操作的总时间摊开来看，每次操作的平均时间仍然是很小的；
 //换句话说就是：看长远，总体是高效的。
 
+//构造
+std::vector<int> v1; //NULL
+std::vector<int> v2(5); //5个0
+std::vector<int> v3(5, 10); //5个10
+std::vector<int> v4 = { 1, 2 ,3 ,4 }; //列表初始化
+
+//遍历
+void func_vector_test()
+{
+	for (int i = 0; i < v4.size(); ++i) std::cout << v4[i] << ' ';
+	
+	for (auto x : v4) std::cout << x << " ";
+
+	for (auto it = v4.begin(); it != v4.end(); ++it) std::cout << *it << ' ';
+}
+
+//指针、引用失效
+//当vector扩容时，它会新建更大的内存并复制旧数据，此时原来的元素指针、引用、迭代器都会失效
+
 
 //常用操作
-//push_back  时间复杂度：O(1)摊还  扩容时为O(n)尾插
+//push_back  时间复杂度：O(1)摊还  扩容时为O(n) 尾插
 //pop_back   O(1)				尾删
 //insert(pos, val)	O(n)		定插
 //erase(pos)	O(n)			定删
@@ -61,4 +81,102 @@ class _vector_
 //所以当存放复杂对象时，建议实现移动构造和移动复制，可以加速扩容和插入
 
 //vector适用场景
+
+//practice1:整数集合类
+//使用std::vector实现一个简单的整数集合类IntSet，支持以下操作：
+//添加整数，删除指定整数，打印所有元素，返回所有整数的和
+class IntSet
+{
+private:
+	std::vector<int> data;
+public:
+	void add(int value)
+	{
+		data.push_back(value);
+	}
+	void remove(int value)
+	{
+		auto it = std::remove(data.begin(), data.end(), value);
+		data.erase(it, data.end());
+	}
+	int sum() const
+	{
+		int s = 0;
+		for (int val : data) s += val;
+		return s;
+	}
+	void print() const
+	{
+		for (int val : data)
+			std::cout << val << " ";
+		std::cout << "\n";
+	}
+};
+//practice2.用vector实现栈
+//要求：实现一个Stack类，支持push，pop, top, empty
+class Stack
+{
+private:
+	std::vector<int> vec;
+public:
+	void push(int value)
+	{
+		vec.push_back(value);
+	}
+	int pop()
+	{
+		if (vec.empty())
+			throw std::out_of_range("Stack is empty");
+		int temp = vec.back();
+		vec.pop_back();
+		return temp;
+	}
+	int top()
+	{
+		if (vec.empty())
+			throw std::out_of_range("Stack is empty");
+		return vec.back();
+	}
+	bool empty()
+	{
+		return vec.empty();
+	}
+};
+
+//practice3 vector模拟学生信息系统
+class StudentSystem
+{
+private:
+	struct Student
+	{
+		std::string name;
+		int age;
+		float score;
+	};
+	std::vector<Student> vec;
+public:
+	void Add(Student student)
+	{
+		vec.push_back(student);
+	}
+	void Delete(std::string name)
+	{
+		for (int i = 0; i < vec.size(); ++i)
+		{
+			if (vec[i].name == name)
+			{
+				vec.erase(vec.begin() + i);
+				return;
+			}
+		}
+		std::cout << "Not Found!" << std::endl;
+	}
+	void print()
+	{
+		for (auto student : vec)
+		{
+			std::cout << student.name << " " << student.age << " " << student.score << std::endl;
+		}
+	}
+};
 
